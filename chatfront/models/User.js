f0dfe90c984e9556f8d9da9099f027b13a1a3757
JABRI Raed <jabriraed80@gmail.com>
2025-02-16 11:26:07 +0100

    add user model

--- chatfront/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const UserSchema = new mongoose.Schema({
-        username: {type: String, required: true, unique: true},
-        password: {type: String, required: true},
-        image:{type:String}
-});
-const userModel = mongoose.model('User', UserSchema);
-export default userModel;
